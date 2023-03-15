1e87a690ea1361b03963b11cc09e20a901e5d2a8
davidedema <demartini.davide01@gmail.com>
2023-03-15 11:00:06 +0100

    created folder

--- deepproblog/minimal/addition_minimal.py
+++ deepproblog/minimal/addition_minimal.py
@@ -1,6 +1,6 @@
 import torch
 
-from data import MNISTImages, AdditionDataset
+from datas import MNISTImages, AdditionDataset
 from deepproblog.dataset import DataLoader
 from deepproblog.engines import ExactEngine
 from deepproblog.model import Model
@@ -15,7 +15,7 @@ net = Network(network, "mnist_net", batching=True)
 # Imposta ottimizzatore per il modello
 net.optimizer = torch.optim.Adam(network.parameters(), lr=1e-3)
 # Crea il modello che contiene la logica e il modello di rete neurale
-model = Model("addition.pl", [net])
+model = Model("/home/davide/Scrivania/progetto_tesi/minimal/addition.pl", [net])
 # Imposta il motore di inferenza
 model.set_engine(ExactEngine(model))
 # Aggiunge i tensori di input con i dati sia train che test
@@ -23,15 +23,20 @@ model.add_tensor_source("train", MNISTImages("train"))
 model.add_tensor_source("test", MNISTImages("test"))
 
 # Crea il dataset con i dati di train
-dataset = AdditionDataset("train")
+dataset = AdditionDataset("test")
 
 # Train the model
 # Crea il dataloader con il dataset e il batch size di 2 e senza shuffle
 loader = DataLoader(dataset, 2, False)
 # Esegue il training del modello 
-train_model(model, loader, 1, log_iter=100, profile=0)
+# train_model(model, loader, 1, log_iter=100, profile=0)
 # Salva lo stato del modello
-model.save_state("snapshot/trained_model.pth")
+# model.save_state("snapshot/trained_model.pth")
+
+# model.load_state(torch.load("/home/davide/Scrivania/progetto_tesi/snapshot/trained_model.pth"))
+model.load_state("/home/davide/Scrivania/progetto_tesi/snapshot/trained_model.pth")
+
+
 
 # Query the model
 query = dataset.to_query(0)
