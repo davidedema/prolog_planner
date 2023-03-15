b67377379b26d2e635a6feae52c4b32c4fdfda90
davidedema <demartini.davide01@gmail.com>
2023-03-15 10:59:53 +0100

    added prolog example

---
+++ prolog/esempio.pl
@@ -0,0 +1,40 @@
+% see(Block, X, Y)
+see(a, 2, 5).
+see(d, 5, 5).
+see(e, 5, 2).
+
+% on(Block, BlockOrTable)
+on(a, b).
+on(b, c).
+on(c, table).
+on(d, table).
+on(e, table).
+
+% trovare z blocco in ricorsiva
+
+z(Block, 0) :-
+    on(Block, table).
+    Coordinate is 1
+
+z(Block, Z) :-
+    on(Block, Block1),
+    z(Block1, Z1),
+    Z is Z1 + 1.
+
+% trovare xy blocchi non visibili
+
+xy(Block, X, Y) :-
+    see(Block, X, Y).
+
+xy(Block, X, Y) :-
+    on(Block1, Block),
+    xy(Block1, X, Y).
+
+% vedere se blocchi sono impilati
+
+up(B1,B2) :-
+    on(B1,B2).
+
+up(B1,B2) :-
+    on(B1,B3),
+    up(B3,B2).
