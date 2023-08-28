e0501b1a79c68f1b9dedbc7254ce448e7dcb22f0
icosac <enricosaccon96@gmail.com>
2023-08-28 14:17:26 +0200

    Added 'inposition()' predicate to decrease the size of plans

--- planner.pl
+++ planner.pl
@@ -28,13 +28,14 @@ plan(State, Goal, _, Moves) :-
 				write('moves are'), nl,
 				reverse_print_stack(Moves).
 plan(State, Goal, Been_list, Moves) :- 	
-				move(Name, Preconditions, Actions),
-				conditions_met(Preconditions, State),
+				move(Name, PreconditionsT, PreconditionsF, Actions),
+				conditions_met(PreconditionsT, State),
+				\+conditions_met(PreconditionsF, State),
 				change_state(State, Actions, Child_state),
 				not(member_state(Child_state, Been_list)),
 				stack(Child_state, Been_list, New_been_list),
 				stack(Name, Moves, New_moves),
-			plan(Child_state, Goal, New_been_list, New_moves),!.
+			plan(Child_state, Goal, New_been_list, New_moves).
 
 change_state(S, [], S).
 change_state(S, [add(P)|T], S_new) :-	change_state(S, T, S2),
@@ -51,6 +52,44 @@ reverse_print_stack(S) :- 	empty_stack(S).
 reverse_print_stack(S) :- 	stack(E, Rest, S), 
 				reverse_print_stack(Rest),
 		 		write(E), nl.
+
+move(
+			grip(A, B), 
+			[available(A), clear(B), on(B, B1)],
+			[inposition(B)],
+			[del(available(A)), del(clear(B)), del(on(B, B1)), add(clear(B1)), add(gripped(A, B))]
+		).
+move(
+			grip(A, B), 
+			[available(A), clear(B), ontable(B)],
+			[inposition(B)],
+			[del(available(A)), del(clear(B)), del(ontable(B)), add(gripped(A, B))]
+		).
+move(
+			move_block(A, B),
+			[gripped(A, B)],
+			[notavalidpredicate(A)],
+			[del(gripped(A, B)), add(moving(A, B))]
+		).
+move(
+			stack(A, B1, B2),
+			[moving(A, B1), clear(B2), inposition(B2)],
+			[notavalidpredicate(A)],
+			[del(moving(A, B1)), add(available(A)), del(clear(B2)), add(on(B1, B2)), add(clear(B1)), 
+				add(inposition(B1)), del(inposition(B2))]
+		).
+move(
+			release(A, B),
+			[moving(A, B)],
+			[notavalidpredicate(A)],
+			[del(moving(A, B)), add(available(A)), add(clear(B)), add(ontable(B)),
+				add(inposition(B))]
+		).
+
+go(S, G) :- plan(S, G, [S], []).
+test :- go([available(a1), available(a2), available(a3), ontable(a), ontable(b), clear(a), clear(b)],
+ 	          [available(a1), available(a2), available(a3), ontable(b), on(a,b), clear(a), inposition(a)]).
+  
 
 
 /* sample moves */
@@ -71,35 +110,6 @@ reverse_print_stack(S) :- 	stack(E, Rest, S),
 % 		[del(holding(X)), del(clear(Y)), add(handempty), add(on(X, Y)),
 % 				  add(clear(X))]).
 
-move(
-			grip(A, B), 
-			[available(A), clear(B), on(B, B1)],
-			[del(available(A)), del(clear(B)), del(on(B, B1)), add(clear(B1)), add(gripped(A, B))]
-		).
-move(
-			grip(A, B), 
-			[available(A), clear(B), ontable(B)],
-			[del(available(A)), del(clear(B)), del(ontable(B)), add(gripped(A, B))]
-		).
-move(
-			move_block(A, B),
-			[gripped(A, B)],
-			[del(gripped(A, B)), add(moving(A, B))]
-		).
-move(
-			release(A, B),
-			[moving(A, B)],
-			[del(moving(A, B)), add(available(A)), add(clear(B)), add(ontable(B))]
-		).
-move(
-			stack(A, B1, B2),
-			[moving(A, B1), clear(B2)],
-			[del(moving(A, B1)), add(available(A)), del(clear(B2)), add(on(B1, B2)), add(clear(B1))]
-		).
 
-go(S, G) :- plan(S, G, [S], []).
-test :- go([available(a1), available(a2), ontable(a), ontable(b), on(c,a), clear(c), clear(b)],
- 	          [available(a1), available(a2), ontable(b), on(a,b), clear(a), ontable(c), clear(c)]).
-  
 testold :- go([handempty, ontable(b), ontable(c), on(a, b), clear(c), clear(a)],
  	          [handempty, ontable(c), on(a,b), on(b, c), clear(a)]).
